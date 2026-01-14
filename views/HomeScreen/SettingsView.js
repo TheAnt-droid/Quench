e8668a9f4c4f4eeeaa48bd05ce42a5832daf2250
Anthea Rebeschini <rebeschini.anthea@gmail.com>
2026-01-14 11:36:56 +0100

    Initial Commit: Added Project to Git

---
+++ views/HomeScreen/SettingsView.js
@@ -0,0 +1,16 @@
+import NavBar from "../../components/Navigation/NavBar.js";
+import TopBar from "../../components/Navigation/TopBar.js";
+import NavMenu from "../../components/Navigation/NavMenu.js";
+const htmlTemplate = /*html*/`
+
+<div>
+  <h1>Settings</h1>
+  <p>Nothing to see here...</p>
+   
+</div>
+`
+
+export default {
+  template: htmlTemplate,
+  components: { NavBar, TopBar, NavMenu }
+};
